1bdef488acb830ccf3b5ee223efc0bd265122873
shouzou-nozaki <soro0530@icloud.com>
2024-11-16 16:35:01 +0900

    feat:初回コミット

---
+++ src/main/java/com/example/devguild_sv/DevguildSvApplication.java
@@ -0,0 +1,16 @@
+package com.example.devguild_sv;
+
+import java.io.Console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevguildSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevguildSvApplication.class, args);
+		System.out.println("起動完了！！");
+	}
+
+}
---
+++ src/test/java/com/example/devguild_sv/DevguildSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devguild_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevguildSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
